4384ed895afe869430accfa2611289db7fac60c5
Weize Kong <294069150@qq.com>
2024-11-01 11:22:14 +0800

    克隆yxc项目骨架

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>Bot名称 : {{ bot_name }}</div>
-  <div>Bot战力 : {{ bot_rating }}</div>
+  <div>
+    <div>Bot昵称：{{ bot_name }}</div>
+    <div>Bot战力：{{ bot_rating }}</div>
+  </div>
+  <router-view></router-view>
 </template>
 
 <script>
@@ -8,15 +11,15 @@ import $ from 'jquery';
 import { ref } from 'vue';
 
 export default {
-  name: 'App',
-  setup() {
+  name: "App",
+  setup: () => {
     let bot_name = ref("");
     let bot_rating = ref("");
+
     $.ajax({
       url: "http://127.0.0.1:3000/pk/getbotinfo/",
-      type: "GET",
+      type: "get",
       success: resp => {
-        console.log(resp);
         bot_name.value = resp.name;
         bot_rating.value = resp.rating;
       }
@@ -25,11 +28,14 @@ export default {
     return {
       bot_name,
       bot_rating
-    };
-  },
-};
+    }
+  }
+}
 </script>
 
-<style scoped>
-
+<style>
+body {
+  background-image: url("@/assets/background.png");
+  background-size: cover;
+}
 </style>
