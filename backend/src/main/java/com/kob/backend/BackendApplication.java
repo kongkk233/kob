d2bdd30eaab920e54e0724d410b5ef9c08f460b5
wzkong <294069150@qq.com>
2024-11-20 23:53:03 +0800

    创建pojo，mapper，usercontroller

--- backend/src/main/java/com/kob/backend/BackendApplication.java
+++ backend/src/main/java/com/kob/backend/BackendApplication.java
@@ -1,9 +1,12 @@
 package com.kob.backend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@MapperScan("com.kob.backend.mapper")
 public class BackendApplication {
 
     public static void main(String[] args) {
